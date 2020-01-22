bf8536e66103c55693b102a3b7433b4cb4bdf3f6
Changhoon Ji <cji2@gmu.edu>
2020-01-21 20:20:32 -0500

    create main app. to convert JSON to Java POJO by Jackson Maven project

---
+++ jackson-databind-json/src/main/java/edu/gmu/jackson/json/Driver.java
@@ -0,0 +1,28 @@
+package edu.gmu.jackson.json;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+			//create object mapper.
+			ObjectMapper mapper = new ObjectMapper();
+			
+			/* read JSON file and map/convert to Java POJO:
+			 * data/sample-lite.json 
+			 */
+			Student aStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			// print first name and last name.
+			System.out.println("First name = " + aStudent.getFirstName());
+			System.out.println("Last name = " + aStudent.getLastName());
+		}
+		catch (Exception exc){
+			exc.printStackTrace();
+		}
+	}
+}
