5a57dbcb7a9a102954480f17ed04b396b50d7a08
Changhoon Ji <cji2@gmu.edu>
2020-01-21 21:32:47 -0500

    modify Student for annotation to ignore unknown properties of JSON file

--- jackson-databind-json/src/main/java/edu/gmu/jackson/json/Student.java
+++ jackson-databind-json/src/main/java/edu/gmu/jackson/json/Student.java
@@ -1,5 +1,8 @@
 package edu.gmu.jackson.json;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Student {
 
 	private int id;
